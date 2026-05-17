bfb7c7c0d69cba117cb5aff3821d6e557d0d2315
Fadilah Puji Laily Maulidiyah <fadilahpjl@gmail.com>
2026-05-17 19:18:38 +0700

    Delete dbt_project/models/gold/gold_peak_hours.sql

--- dbt_project/models/gold/gold_peak_hours.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    order_date,
-    order_hour,
-    COUNT(order_id) AS total_transactions
-FROM {{ ref('silver_transactions') }}
-GROUP BY order_date, order_hour
-ORDER BY order_date, order_hour
